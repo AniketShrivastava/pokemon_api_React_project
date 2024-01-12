09df35b8613e24182a4a912ea5d1cc2886d1b758
Aniket shrivastava <aniket.shrivastava91@gmail.com>
2024-01-12 23:56:57 +0530

    update pokemon component

--- src/Pokemon/Pokemon.jsx
+++ src/Pokemon/Pokemon.jsx
@@ -1,14 +1,17 @@
+import { Link } from 'react-router-dom';
 import './Pokemon.css'
-function Pokemon({name,type,url}){
-  return(
-    <>
-    <div className='pokemon'>
-    <div className='pokemon-name'>{name}</div>
-    
-    <img className='pokemon-image' src={url}/>
-    
-    </div>
-    </>
-  )
+function Pokemon({ name, url, id }) {
+    return (
+        <>
+            <Link to={`/pokemon/${id}`} className='pokemon-wrappers'>
+                <div className='pokemon'>
+                    <div className='pokemon-name'>{name}</div>
+                    <img className='pokemon-image' src={url} />
+                </div>
+            </Link>
+
+        </>
+    )
 }
+
 export default Pokemon;
